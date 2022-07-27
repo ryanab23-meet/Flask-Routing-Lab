c1adb68b73319f7d9356ace71f0211451afd70d0
rryannnn111 <94308251+rryannnn111@users.noreply.github.com>
2022-07-27 17:01:08 +0300

    app.py

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -10,9 +10,26 @@ app = Flask(  # Create a flask app
 # Your code should be below
 @app.route('/')
 def home():
-    return render_template("home.html")
-    
-# Your code should be above
+    products = ["Milk,egg,bread,meat,"]
+    return render_template(
+    "home.html",products="products")
 
+@app.route('/milk')
+def milk():
+    return render_template('milk.html')
+
+@app.route('/egg')
+def egg():
+    return render_template('egg.html')
+
+@app.route('/bread')
+def bread():
+    return render_template('bread.html')
+
+@app.route('/meat')
+def meat():
+    return render_template('meat.html')    
+#Your code should be above 
+ 
 if __name__ == "__main__":  # Makes sure this is the main process
     app.run(debug=True)
